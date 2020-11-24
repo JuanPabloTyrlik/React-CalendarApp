1aeb387b148c41a7fc90c51e000bda2030489eee
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-11-24 19:36:11 -0300

    Calendar Screen and Event

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
+import 'moment/locale/es';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { CalendarEvent } from './CalendarEvent';
 
+moment.locale('es');
 const localizer = momentLocalizer(moment);
 
 const events = [
@@ -13,10 +17,44 @@ const events = [
         start: moment().toDate(),
         end: moment().add(2, 'hours').toDate(),
         bgcolor: '#FAFAFA',
+        notes: 'Comprar torta',
+        user: {
+            name: 'Juan Pablo',
+        },
     },
 ];
 
 export const CalendarScreen = () => {
+    const [lastView, setLastView] = useState(
+        localStorage.getItem('lastView') || 'month'
+    );
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white',
+        };
+        return {
+            style,
+        };
+    };
+
+    const onDoubleClick = (e) => {
+        console.log(e);
+    };
+
+    const onSelectEvent = (e) => {
+        console.log(e);
+    };
+
+    const onViewChange = (e) => {
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    };
+
     return (
         <div className="calendar-screen">
             <Navbar />
@@ -25,7 +63,15 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
-                style={{ height: 500 }}
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelectEvent}
+                onView={onViewChange}
+                view={lastView}
+                components={{
+                    event: CalendarEvent,
+                }}
             />
         </div>
     );
